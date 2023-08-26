9b2777602c0f1fbaca43517b262d166d89297210
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2023-08-26 21:09:01 +0700

    add: 00_intro

---
+++ grader/00_Intro/00_Intro_★_Big_Numbers.py
@@ -0,0 +1,10 @@
+# Please apply your effort before copying, pasting, and converting to your own version.
+
+x = 2938402734091273094162387451928736401926340971234
+y = 9208209384928743098273495872039847509273497
+
+print(x)
+print("+")
+print(y)
+print("=")
+print(x+y)
---
+++ grader/00_Intro/00_Intro_★_Hello.py
@@ -0,0 +1,4 @@
+# Please apply your effort before copying, pasting, and converting to your own version.
+
+print("Hello Python.")
+print("We're using Python 3.")
